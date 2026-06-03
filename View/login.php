e948aaa8c4cbb51fd0849bd991b4ff8cba2ab7d7
Jose Cardinale <joseelias.velasquez@hotmail.com>
2026-06-03 01:01:19 -0600

    Primer Avance

--- View/login.php
+++ View/login.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="es">}
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    
 
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -18,11 +19,8 @@
         <nav class="navbar navbar-expand-lg navbar-white bg-primary  border-bottom">
             <div class="container">
                 <a class="navbar-brand fw-bold text-white" href="index.php">
-                    Ultras.
+                    Tienda Ropa
                 </a>
-
-               
-                </div>
             </div>
         </nav>
     </header>
