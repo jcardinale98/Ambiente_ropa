8defae4c2d99e726c68bac0a9d2087c7903a0f6d
Antonio-MT <fmorales10696@ufide.ac.cr>
2026-07-15 18:29:17 -0600

    bd
    se coloco el nombre de la bd correcta

--- Model/UtilitarioModel.php
+++ Model/UtilitarioModel.php
@@ -7,7 +7,7 @@
     function OpenDB()
     {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        return new mysqli("127.0.0.1:3307", "root", "", "mn");
+        return new mysqli("127.0.0.1:3307", "root", "", "bdproyecto");
     }
 
     function CloseDB($conn)
